3361c8923135447d093ac6b45e89a817e56c8975
Mateo <mateo.avalos3942@gmail.com>
2024-08-20 11:30:35 -0500

    Added controllers for product and customer

---
+++ SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/controllers/CustomerController.java
@@ -0,0 +1,70 @@
+package com.exercise.SpringBoot.Exercise.controllers;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.exercise.SpringBoot.Exercise.modelEntity.Customer;
+import com.exercise.SpringBoot.Exercise.services.ICustomerService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping("/findAll")
+    public List<Customer> getCustomers() {
+        return customerService.findAll();
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveCustomer(@Valid @RequestBody Customer customer) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            customerService.save(customer);
+        } catch (Exception e) {
+            response.put("message", "Error: " + e.getMessage());
+            return ResponseEntity.status(500).body(response);
+        }
+        response.put("message", "Customer saved successfully");
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteById(id);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> getCustomer(@PathVariable Long id) {
+        Customer customer = customerService.findById(id);
+        Map<String, String> response = new HashMap<>();
+        if (customer == null) {
+            response.put("message", "Customer not found");
+            return ResponseEntity.status(404).body(response);
+        }
+        return ResponseEntity.status(200).body(customer);
+    }
+
+    @PutMapping("/update/{id}")
+    public Customer updateCustomer(@RequestBody Customer customer, @PathVariable Long id) {
+        Customer customerToUpdate = customerService.findById(id);
+        if(customerToUpdate == null)
+            throw new RuntimeException("Customer not found");
+        customerToUpdate.setName(customer.getName());
+        customerToUpdate.setEmail(customer.getEmail());
+        customerToUpdate.setPhone(customer.getPhone());
+        customerToUpdate.setAddress(customer.getAddress());
+        customerToUpdate.setPurchase(customer.getPurchase());
+        return customerService.save(customerToUpdate);
+    }
+}
---
+++ SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/controllers/ProductController.java
@@ -0,0 +1,71 @@
+package com.exercise.SpringBoot.Exercise.controllers;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exercise.SpringBoot.Exercise.modelEntity.Product;
+import com.exercise.SpringBoot.Exercise.services.IProductService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping("/findAll")
+    public List<Product> getProducts() {
+        return productService.findAll();
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveProduct(@Valid @RequestBody Product product) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            productService.save(product);
+        } catch (Exception e) {
+            response.put("message", "Error: " + e.getMessage());
+            return ResponseEntity.status(500).body(response);
+        }
+        response.put("message", "Product saved successfully");
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        Map<String, String> response = new HashMap<>();
+        if (product == null) {
+            response.put("message", "Product not found");
+            return ResponseEntity.status(404).body(response);
+        }
+        return ResponseEntity.status(200).body(product);
+    }
+
+    @PutMapping("/update/{id}")
+    public Product updateProduct(@RequestBody Product product, @PathVariable Long id) {
+        Product productToUpdate = productService.findById(id);
+        if(productToUpdate == null)
+            throw new RuntimeException("Product not found");
+        productToUpdate.setName(product.getName());
+        productToUpdate.setDescription(product.getDescription());
+        productToUpdate.setPrice(product.getPrice());
+        productToUpdate.setStock(product.getStock());
+        return productService.save(productToUpdate);
+    }
+}
