648fa47291a2b94df4d42c3fdb4908d81653bd9d
Mateo <mateo.avalos3942@gmail.com>
2024-08-31 18:50:33 -0500

    Improved save method

--- SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/Purchase/services/PurchaseService.java
+++ SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/Purchase/services/PurchaseService.java
@@ -3,6 +3,9 @@ package com.exercise.SpringBoot.Exercise.Purchase.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.exercise.SpringBoot.Exercise.Customer.dao.ICustomerDAO;
+import com.exercise.SpringBoot.Exercise.Product.dao.IProductDAO;
+import com.exercise.SpringBoot.Exercise.Product.modelEntity.Product;
 import com.exercise.SpringBoot.Exercise.Purchase.dao.IPurchaseDAO;
 import com.exercise.SpringBoot.Exercise.Purchase.modelEntity.Purchase;
 
@@ -11,6 +14,12 @@ public class PurchaseService implements IPurchaseService {
     @Autowired
     private IPurchaseDAO purchaseDAO;
 
+    @Autowired
+    private ICustomerDAO customerDAO;
+
+    @Autowired
+    private IProductDAO productDAO;
+
     @Override
     public List<Purchase> findAll() {
         return purchaseDAO.findAll();
@@ -18,6 +27,18 @@ public class PurchaseService implements IPurchaseService {
 
     @Override
     public Purchase save(Purchase purchase) {
+        if(purchase.getCustomer() == null || customerDAO.findById(purchase.getCustomer().getId()).isEmpty())
+            throw new RuntimeException("Customer not found");
+        
+        Product productToUpdate = productDAO.findById(purchase.getProduct().getId()).orElse(null);
+        if(productToUpdate == null)
+            throw new RuntimeException("Product not found");
+        if(productToUpdate.getStock() < purchase.getQuantity())
+            throw new RuntimeException("Not enough stock");
+        productToUpdate.setStock(productToUpdate.getStock() - purchase.getQuantity());
+        productDAO.save(productToUpdate);
+
+        purchase.setDate(java.time.LocalDateTime.now().toString());
         return purchaseDAO.save(purchase);
     }
 
