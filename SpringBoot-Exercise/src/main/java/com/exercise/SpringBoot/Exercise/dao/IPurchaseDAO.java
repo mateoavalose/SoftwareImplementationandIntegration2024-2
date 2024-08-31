f7bc2c79bba8c3df8380b0ad13077f5b1a69940a
Mateo <mateo.avalos3942@gmail.com>
2024-08-31 17:55:32 -0500

    Added query. Not tested

--- SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/dao/IPurchaseDAO.java
+++ SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/dao/IPurchaseDAO.java
@@ -1,8 +1,13 @@
 package com.exercise.SpringBoot.Exercise.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.exercise.SpringBoot.Exercise.modelEntity.Purchase;
 
 public interface IPurchaseDAO extends JpaRepository<Purchase, Long> {
-    
+    @Query("SELECT p FROM Purchase p WHERE p.customer_id = :customer_id")
+    public List<Purchase> getCustomerOrders(@Param("customer_id") Long customer_id);
 }
