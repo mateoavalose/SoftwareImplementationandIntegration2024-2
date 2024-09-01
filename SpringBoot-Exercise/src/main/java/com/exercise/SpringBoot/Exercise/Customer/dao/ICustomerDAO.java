a3042795be206e419213732a18c3d3e7d935e307
Mateo <mateo.avalos3942@gmail.com>
2024-09-01 12:15:38 -0500

    Deleted unimplemented methods

--- SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/Customer/dao/ICustomerDAO.java
+++ SpringBoot-Exercise/src/main/java/com/exercise/SpringBoot/Exercise/Customer/dao/ICustomerDAO.java
@@ -1,13 +1,7 @@
 package com.exercise.SpringBoot.Exercise.Customer.dao;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.exercise.SpringBoot.Exercise.Customer.modelEntity.Customer;
 
 public interface ICustomerDAO extends JpaRepository<Customer, Long> {
-    public List<Customer> findByName(String name);
-
-    public List<Customer> findByEmail(String email);
-    
-    public List<Customer> findByPhone(Integer phone);
 }
