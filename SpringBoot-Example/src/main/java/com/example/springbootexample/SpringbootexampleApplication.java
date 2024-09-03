6ec522717d56e219adff54374ba7cdfd9d716286
Mateo <mateo.avalos3942@gmail.com>
2024-09-03 11:38:26 -0500

    Added .env and added dependencies for JWT security

--- SpringBoot-Example/src/main/java/com/example/springbootexample/SpringbootexampleApplication.java
+++ SpringBoot-Example/src/main/java/com/example/springbootexample/SpringbootexampleApplication.java
@@ -2,10 +2,17 @@ package com.example.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SpringbootexampleApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		// Programmatically set system properties so Spring can access them
+		System.setProperty("CONNECTION_URL", dotenv.get("CONNECTION_URL"));
+		System.setProperty("USER", dotenv.get("USER"));
+		System.setProperty("PASSWORD", dotenv.get("PASSWORD"));
+
 		SpringApplication.run(SpringbootexampleApplication.class, args);
 	}
 }
