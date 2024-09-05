10b07baf58b4a29e9852861add3e993df44dbfb6
Mateo <mateo.avalos3942@gmail.com>
2024-09-05 12:04:05 -0500

    Added FashionShow SpringBoot blank project to develop the parcial exam

---
+++ src/test/java/com/FashionShow/FashionShow_Project/FashionShowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FashionShow.FashionShow_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionShowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
