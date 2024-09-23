81137acb91fe2e9accd51d37796e0e243f4d0611
Mateo <mateo.avalos3942@gmail.com>
2024-09-23 14:45:51 -0500

    Added .env implementation and connection to database

--- src/main/java/com/FashionShow/FashionShow_Project/FashionShowProjectApplication.java
+++ src/main/java/com/FashionShow/FashionShow_Project/FashionShowProjectApplication.java
@@ -3,10 +3,17 @@ package com.FashionShow.FashionShow_Project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FashionShowProjectApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("CONNECTION_URL", dotenv.get("CONNECTION_URL"));
+		System.setProperty("USER", dotenv.get("USER"));
+		System.setProperty("PASSWORD", dotenv.get("PASSWORD"));
+
 		SpringApplication.run(FashionShowProjectApplication.class, args);
 	}
 
